daae5052bea898d87b3c120f814e5b234f36f08c
DotStarMoney <Batterybat@gmail.com>
2021-09-26 20:37:30 -0400

    Update main.cc

--- fake_landsatz_2d/main.cc
+++ fake_landsatz_2d/main.cc
@@ -6,16 +6,16 @@
 static const char blox[] = {219, 219};
 
 int main(int argc, char** argv) { 
-	auto cool = nbd::BreakPattern(5, 5);
-	
-	int offset = 0;
+  auto cool = nbd::BreakPattern(5, 5);
+  
+  int offset = 0;
   for (int y = 0; y < 5; ++y) {
     for (int x = 0; x < 5; ++x) {
       std::cout << (cool.get()[offset++] ? blox : "  ");
-		}      
-		std::cout << std::endl;
-	}
+    }      
+    std::cout << std::endl;
+  }
   std::cout << std::endl;
 
-	return 0; 
+  return 0; 
 }
